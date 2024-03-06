3cdbb8b213685d5928743e7578a39125b982c804
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-06 17:55:42 +0330

    required the username and change only username Instead only email

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -29,7 +29,7 @@ class LoginRequest extends FormRequest
     {
         return [
             'email' => ['nullable', 'string', 'email'],
-            'username' => ['nullable', 'string', 'alpha_dash:ascii', 'max:20'],
+            'username' => ['required', 'string', 'alpha_dash:ascii', 'max:20'],
             'password' => ['required', 'string'],
         ];
     }
@@ -43,7 +43,7 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if (! Auth::attempt($this->only('username', 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
