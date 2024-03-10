2d27b2eb8ca30a30fd4c1ad380db41af07574dac
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-10 19:32:49 +0330

    whrite the site layout

---
+++ resources/views/layouts/site.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name') }}</title>
+
+    @vite(['resources/css/app.css', 'resource/js/app.js'])
+
+</head>
+<body class="font-sans text-gray-900 antialiased max-w-md mx-auto">
+
+    <x-header/>
+    
+</body>
+</html>
