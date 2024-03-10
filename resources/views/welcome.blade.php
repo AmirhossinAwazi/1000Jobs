588ede8d991bc692bcd9e809c7665e78ad161c43
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-10 19:57:59 +0330

    change the layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html dir="rtl" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{ config('app.name') }}</title>
-
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    </head>
-    <body class="antialiased max-w-md mx-auto">
-        <x-header/>
+<x-site-layout>
+    <div class="antialiased max-w-md mx-auto">
         <div class="flex justify-between border-b border-slate-300">
             <div class="flex mt-2 pr-2">
                 <div>
@@ -24,5 +13,5 @@
                 <x-search-svg/>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</x-site-layout>
