26510a764751d7c25b8ed508d8f11453a5ede898
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-11 19:33:34 +0330

    create header for SAQ

---
+++ resources/views/components/header-SAQ.blade.php
@@ -0,0 +1,22 @@
+<div class="bg-slate-300 p-4">
+    <div class="grid grid-cols-3">
+        <div class="flex space-x-2 space-x-reverse items-center">
+            <x-pensel-svg />
+            <h1>
+                فرم معرفی شغل
+            </h1>
+        </div>
+        <div class="bg-white rounded-lg p-0.5 flex space-x-1 space-x-reverse justify-between text-neutral-400 border shadow-sm">
+            <div class="rounded-lg px-4 ">
+                1
+            </div>
+            <div class="rounded-lg px-4 bg-neutral-400/50 text-neutral-700">
+                2
+            </div>
+            <div class="rounded-lg px-4">
+                3
+            </div>
+        </div>
+        <div></div>
+    </div>
+</div>
