055e0caecef2a12649113b6b6f3f716a4b8639f8
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-14 10:53:01 +0330

    SVG

---
+++ resources/views/components/add-photo-svg.blade.php
@@ -0,0 +1,5 @@
+<svg width="80" height="80" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="2.5" y="2.5" width="195" height="195" rx="27.5" fill="white" stroke="#A3A3A3" stroke-width="5"/>
+    <path d="M100 75L100 125" stroke="#A3A3A3" stroke-width="5" stroke-linecap="round"/>
+    <path d="M75 100L125 100" stroke="#A3A3A3" stroke-width="5" stroke-linecap="round"/>
+    </svg>    
