26d1ab80172a7d7781cbb08926773b865c00c9b6
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-15 21:35:51 +0330

    change tha method

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () { return view('welcome'); })->name('home');
 
 // Route::resource('/SAQ', JobController::class);
-Route::get('/SAQ', [JobController::class, 'index'])->name('Job.saq');
+Route::get('/SAQ', [JobController::class, 'create'])->name('Job.saq');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
