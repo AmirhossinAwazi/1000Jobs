dc7cd4dcdd6d8dc249c6aa036d282e10e5039d58
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-17 13:40:23 +0330

    delete user_id table from caregories

--- database/migrations/2024_03_05_084356_create_categories_table.php
+++ database/migrations/2024_03_05_084356_create_categories_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('title')->unique();
-            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
