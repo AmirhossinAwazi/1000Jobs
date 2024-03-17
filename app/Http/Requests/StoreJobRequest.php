4414be5cc41f44140f38145e4ee1d6559dea5633
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-17 13:33:45 +0330

    add the new validation for StoreJobRequest

--- app/Http/Requests/StoreJobRequest.php
+++ app/Http/Requests/StoreJobRequest.php
@@ -6,22 +6,34 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreJobRequest extends FormRequest
 {
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
     public function rules(): array
     {
         return [
-            'title' => 'required|string|max:45',
-            'experience-years' => 'required|integer',
-            'job-datisfaction' => 'required|in:1,2,3',
-            'min-income-range' => 'required|integer',
-            'max-income-range' => 'required|integer',
-            'revenue-model' => 'required',
-            'company' => 'nullable|string|max:50',
-            'description' => 'required|string',
-            'photo' => 'required|image|mimes:jpeg,png,jpg|max:2048',
-            'name' => 'required|string|max:45',
-            'needs-advice' => 'nullable|boolean',
-            'Phone-number' => 'required|string|regex:/^(0|98|\+98)?(9\d{9}$)/',
+            'category_id' => ['required', 'exists:categories,id'],
+            'experience_years' => ['required', 'integer'],
+            'job_datisfaction' => ['required', 'in:1,2,3'],
+            'min_income_range' => ['required', 'integer'],
+            'max_income_range' => ['required', 'integer'],
+            'revenue_model' => ['required'],
+            'company' => ['nullable', 'string', 'max:50'],
+            'description' => ['required', 'string'],
+            'photo' => ['image', 'mimes:jpeg,png,jpg', 'max:2048'],
+            'name' => ['required', 'string', 'max:45'],
+            'needs_advice' => ['nullable', 'boolean'],
+            'phone_number' => ['required', 'string', 'regex:/^(\+?98|0)?9\d{9}$/']
         ];
     }
 }
-
