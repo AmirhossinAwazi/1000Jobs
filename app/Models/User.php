d490a8a2c59ea73b255ffedaa73fe31848d77f9b
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-17 13:38:33 +0330

    change the fillable and write relation beetween job and user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -19,9 +20,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'username',
-        'email',
-        'password',
+        'phone_number',
+        'needs_advice',
     ];
 
     /**
@@ -43,4 +43,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function job():HasOne{
+        return $this->hasOne(Job::class);
+    }
 }
