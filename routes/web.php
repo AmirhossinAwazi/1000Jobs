189695896ffdb26507ca82b1ea82a7d6b78522fd
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-17 13:46:31 +0330

    change route get to resourse for /SAQ

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () { return view('welcome'); })->name('home');
 
-// Route::resource('/SAQ', JobController::class);
-Route::get('/SAQ', [JobController::class, 'create'])->name('Job.saq');
+Route::resource('/SAQ', JobController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
