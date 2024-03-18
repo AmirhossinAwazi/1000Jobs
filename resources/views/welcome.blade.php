35d0d20a2f2c04a16e22b0f05245e788620ff71d
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-18 18:36:04 +0330

    show jobs in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,39 @@
         <div class="flex justify-between border-b border-slate-300">
             <div class="flex mt-2 pr-2">
                 <div>
-                    <x-pensel-svg/>
+                    <x-pensel-svg />
                 </div>
                 <div class="font-semibold pr-2">
                     فهرست مشاغل
                 </div>
             </div>
             <div class="flex justify-items-start pl-2 mt-2 text-lg">
-                <x-search-svg/>
+                <x-search-svg />
             </div>
         </div>
+    </div>
+
+    <div>
+        @foreach ($jobs as $job)
+            <div class="w-full h-80 bg-white border-2 border-zinc-100">
+
+                <div class="flex w-64 h-64 left-[22px] top-[17px] rounded-2xl">
+                    <img src="{{ $job->photo }}" alt="{{ $job->category->title }}">
+                </div>
+
+                <div class="flex justify-between p-3">
+                    <div class="text-right text-black text-lg font-bold">
+                        <p>{{ $job->category->title }}</p>
+                    </div>
+
+                    <div class="flex text-neutral-600 text-sm font-bold">
+                        {{ $job->min_income_range }}
+                        <p>تا</p>
+                        {{ $job->max_income_range }}
+                        <p>م تومن</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 </x-site-layout>
