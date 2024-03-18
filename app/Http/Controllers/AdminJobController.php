acc9babff28da48e5785adfd2c7ea40ac9dfe34f
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-18 11:07:39 +0330

    delete admin fulder in controller and create new admin controller in the controller folder

--- app/Http/Controllers/AdminJobController.php
+++ app/Http/Controllers/AdminJobController.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class JobController extends Controller
+class AdminJobController extends Controller
 {
     /**
      * Display a listing of the resource.
