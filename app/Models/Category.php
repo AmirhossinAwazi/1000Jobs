8381ad2df7d26d31a39131bde3c05e2e88dfcf01
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-18 18:32:07 +0330

    add the new relation betwin category and job

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title'];
+    protected $fillable = [
+        'title',
+    ];
+
+    // public function job():HasMany{
+    //     return $this->hasMany(job::class);
+    // }
+
+    public function job():BelongsToMany{
+        return $this->belongsToMany(job::class);
+    }
 }
