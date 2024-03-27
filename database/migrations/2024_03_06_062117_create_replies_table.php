3867c5027b59405f15120d91026a4170321e4af5
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-27 11:21:01 +0330

    get the name for comments and replies

--- database/migrations/2024_03_06_062117_create_replies_table.php
+++ database/migrations/2024_03_06_062117_create_replies_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('replies', function (Blueprint $table) {
             $table->id();
             $table->text('body');
+            $table->string('name');
             $table->dateTime('date_written')->nullable();
             $table->foreignId('job_id')->constrained();
             $table->unsignedBigInteger('user_id');
