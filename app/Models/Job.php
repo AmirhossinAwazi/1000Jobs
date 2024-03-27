d34d92d6f7272f478116bf0b9a2fe70412c0ba07
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-03-27 16:48:39 +0330

    create new relation at the job and user model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
@@ -39,6 +40,10 @@ class Job extends Model implements HasMedia
 
     public function user():BelongsTo{
         return $this->belongsTo(user::class);
+    }
+
+    public function comments():HasMany{
+        return $this->hasMany(Comment::class);
     }
 
 }
