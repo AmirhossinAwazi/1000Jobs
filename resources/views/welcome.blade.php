b08f159122aba39e74547197712a6c3f039fea05
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-04-07 12:33:10 +0330

    create 4 jobs in the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,30 +15,100 @@
         </div>
     </div>
 
-    <div>
-        @foreach ($jobs as $job)
-            <a href="{{ route('view', ['job' => $job->id]) }}" class="w-full bg-white border-2 border-zinc-100">
-                <div class="flex">
-                    <img class="w-full h-full object-cover rounded-2xl" src="{{ $job->photo?->original_url }}"
-                        alt="{{ $job->category->title }}">
+    <div class="mt-5">
+        <a href="#">
+            <div>
+                <img src="{{ asset('storage/images/developer.jpg') }}" alt="برنامه نویس"
+                    class="w-full h-full object-cover rounded-2xl">
+            </div>
+
+            <div class="flex justify-between items-center p-3">
+                <div>
+                    <p class="text-right text-black text-lg font-bold">برنامه نویس</p>
+                    <p class="text-neutral-400 text-sm font-normal">5 معرفی</p>
                 </div>
 
-                <div class="flex justify-between items-center p-3">
-                    <div class="text-right text-black text-lg font-bold">
-                        <div class="flex gap-2 items-center">
-                            <p>{{ $job->user->name }}</p>
-                            <p>{{ $job->category->title }}</p>
-                        </div>
-                    </div>
-
-                    <div class="flex text-neutral-600 text-sm font-bold">
-                        {{ $job->min_income_range }}
-                        <p>تا</p>
-                        {{ $job->max_income_range }}
-                        <p>م تومن</p>
-                    </div>
+                <div class="flex text-neutral-600 text-sm font-bold mt-5">
+                    <p>10 تا 45 م تومن</p>
                 </div>
-            </a>
-        @endforeach
+            </div>
+
+        </a>
+
+        <a href="#">
+            <div>
+                <img src="{{ asset('storage/images/lawyer.jpg') }}" alt="وکیل"
+                    class="w-full h-64 object-cover rounded-2xl">
+            </div>
+
+            <div class="flex justify-between items-center p-3">
+                <div>
+                    <p class="text-right text-black text-lg font-bold">وکیل</p>
+                    <p class="text-neutral-400 text-sm font-normal">4 معرفی</p>
+                </div>
+
+                <div class="flex text-neutral-600 text-sm font-bold mt-5">
+                    <p>12 تا 20 م تومن</p>
+                </div>
+            </div>
+        </a>
+
+        <a href="#">
+            <div>
+                <img src="{{ asset('storage/images/teacher.jpg') }}" alt="معلم"
+                    class="w-full h-full object-cover rounded-2xl">
+            </div>
+
+            <div class="flex justify-between items-center p-3">
+                <div>
+                    <p class="text-right text-black text-lg font-bold">معلم</p>
+                    <p class="text-neutral-400 text-sm font-normal">2 معرفی</p>
+                </div>
+
+
+                <div class="flex text-neutral-600 text-sm font-bold mt-5">
+                    <p>5 تا 15 م تومن</p>
+                </div>
+            </div>
+        </a>
+
+        <a href="#">
+            <div>
+                <img src="{{ asset('storage/images/seller.jpg') }}" alt="فروشنده"
+                    class="w-full h-full object-cover rounded-2xl">
+            </div>
+
+            <div class="flex justify-between items-center p-3">
+                <div>
+                    <p class="text-right text-black text-lg font-bold">فروشنده</p>
+                    <p class="text-neutral-400 text-sm font-normal">15 معرفی</p>
+                </div>
+
+                <div class="flex text-neutral-600 text-sm font-bold mt-5">
+                    <p>20 تا 60 م تومن</p>
+                </div>
+            </div>
+
+        </a>
+
+        <a href="#">
+            <div>
+                <img src="{{ asset('storage/images/translator.png') }}" alt="مترجم"
+                    class="w-full h-full object-cover rounded-2xl">
+            </div>
+
+            <div class="flex justify-between items-center p-3">
+                <div>
+                    <p class="text-right text-black text-lg font-bold">مترجم</p>
+                    <p class="text-neutral-400 text-sm font-normal">9 معرفی</p>
+                </div>
+
+                <div class="flex text-neutral-600 text-sm font-bold mt-5">
+                    <p>80 تا 100 م تومن</p>
+                </div>
+            </div>
+
+        </a>
     </div>
+
 </x-site-layout>
