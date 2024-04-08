f4b902ed4029a3965171c0c3463b91bd5c18f3c0
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-04-08 09:46:17 +0330

    new route for categories

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminJobController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\ProfileController;
@@ -9,11 +10,13 @@ use App\Http\Controllers\ViewController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/categories/{category}', [SiteController::class, 'show'])->name('category');
+// Route::get('/categories/{category}', [CategoryController::class, 'index'])->name('category');
 
-Route::get('/', [SiteController::class, 'index'])->name('home');
+// Route::get('/categories/{category}', [SiteController::class, 'show'])->name('category');
 
-Route::get('/view/{job}', [ViewController::class,'show'])->name('view');
+Route::get('/', [SiteController::class, 'show'])->name('home');
+
+Route::get('/category/{Category:id}', [ViewController::class,'show'])->name('view');
 
 Route::resource('/job/{job}/comment', CommentController::class);
 
