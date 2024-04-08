6e492c166006d8d2dac2b3c83acab7bd54610800
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-04-08 09:45:50 +0330

    new code in show method in the site controller for data categorues

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -14,9 +14,10 @@ class SiteController extends Controller
     public function index()
     {
         $jobs = Job::with('photo')->get();
+        $categories = Category::get();
         return view('welcome', [
             'jobs' => $jobs,
-            'categories' => Category::all(),
+            'categories' => $categories,
         ]);
     }
 
@@ -39,9 +40,51 @@ class SiteController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show()
     {
-        //
+        // $categories = Category::get();
+        // return view('welcome', [
+        //     'categories' => $categories,
+        // ]);
+        $categories = [
+            [
+                'id' => 1,
+                'title' => 'برنامه‌نویس',
+                'image' => 'developer.jpg',
+                'text' => 'بین ۷ تا ۴۵ میلیون تومان ماهانه',
+            ],
+
+            [
+                'id' => 2,
+                'title' => 'وکیل',
+                'image' => 'lawyer.jpg',
+                'text' => 'بین ۲۰ تا ۴۰۰ میلیون ماهانه',
+            ],
+
+            [
+                'id' => 3,
+                'title' => 'معلم',
+                'image' => 'teacher.jpg',
+                'text' => 'بین ۶ تا ۲۵ میلیون ماهانه',
+            ],
+
+            [
+                'id' => 4,
+                'title' => 'فروشنده',
+                'image' => 'seller.jpg',
+                'text' => 'بین ۴ تا ۵۰ میلیون ماهانه',
+            ],
+
+            [
+                'id' => 5,
+                'title' => 'مترجم',
+                'image' => 'translator.jpg',
+                'text' => 'بین ۷ تا ۲۰ میلیون ماهانه',
+            ],
+        ];
+        return view('welcome', [
+            'categories' => $categories,
+        ]);
     }
 
     /**
