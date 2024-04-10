06d7d0b8e68724e819996454e6b166fdcef80014
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-04-10 15:33:29 +0330

    create seeder for at glance and att to date basa seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(CategorySeeder::class);
         $this->call(AdminSeeder::class);
+        $this->call(AtGlancesSeeder::class);
         
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
