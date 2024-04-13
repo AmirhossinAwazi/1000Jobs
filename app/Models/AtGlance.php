efe9e4940404aede349d0057fe3b9124c44a45a9
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-04-13 10:18:42 +0330

    create model for at glamce

--- app/Models/AtGlance.php
+++ app/Models/AtGlance.php
@@ -8,7 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class AtGlance extends Model
 {
     use HasFactory;
+    
+    protected $table = 'atglances';
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'category_id',
         'description',
@@ -16,4 +23,15 @@ class AtGlance extends Model
         'skill',
         'investment',
     ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'skill' => 'array',
+    ];
+
+    // You can define any additional relationships or methods here as needed
 }
