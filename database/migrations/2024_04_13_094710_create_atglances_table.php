50df7d463c289f5b92aa8c40f185584d432f2d4c
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-04-14 16:44:32 +0330

    add the name to atglance schima

--- database/migrations/2024_04_13_094710_create_atglances_table.php
+++ database/migrations/2024_04_13_094710_create_atglances_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('atglances', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained();
+            $table->string('name');
             $table->longText('description');
             $table->string('evidence');
             $table->text('skill');
