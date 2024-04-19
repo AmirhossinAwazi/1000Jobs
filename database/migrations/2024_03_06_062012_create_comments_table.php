90f3ecfd673f632cf3b6536cf41ea2664a92ad99
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-04-19 21:45:46 +0330

    change the job_id to category_id in migration comments

--- database/migrations/2024_03_06_062012_create_comments_table.php
+++ database/migrations/2024_03_06_062012_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('parent_id')->nullable();
-            $table->foreignId('job_id')->constrained();
+            $table->foreignId('category_id')->constrained();
             $table->text('body');
             $table->string('lastname');
             $table->foreign('parent_id')->references('id')->on('comments')->onDelete('cascade');
